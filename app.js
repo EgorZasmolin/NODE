3097f4d2b9230a122bb52b70bd6212fc073be47d
= <=>
2023-10-17 13:41:20 +0300

    commit

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const path = require(path);
+const path = require("path");
+const fs = require("fs");
 
-console.log("Название файла: ", path.basename(__filename));
+// console.log("Название файла: ", path.basename(__filename));
+// console.log("Название каталога: ", path.dirname(__filename));
+
+fs.mkdir(path.join(__dirname, "tmp"), function (err) {
+  if (err) {
+    console.error(err);
+  }
+  console.log("Папка создана");
+});
