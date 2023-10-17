203e3ef01cd42521b44fa82a6ee30a7ceb45dbc7
= <=>
2023-10-17 15:04:24 +0300

    commit

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-const path = require("path");
-const fs = require("fs");
+import express from "express";
+const app = express();
+const port = "3000";
 
-// console.log("Название файла: ", path.basename(__filename));
-// console.log("Название каталога: ", path.dirname(__filename));
-
-// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log("Папка создана");
-// });
-
-const filePath = path.join(__dirname, "tmp", "2.txt");
-console.log(filePath);
-fs.writeFile(filePath, "Something wrong in your file", function (err) {
-  if (err) {
-    console.error(err);
-  }
-  console.log("Папка создана");
+app.get("/as", function (req, res) {
+  res.end("Hello Students!!");
 });
-
-fs.readFile(filePath, "UTF-8", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data);
+app.listen(port, () => {
+  console.log("Listening on port: " + port);
 });
