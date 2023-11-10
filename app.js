fb8c2e8b916f35ba035bcd1ea3489d2e7c4b9b32
= <=>
2023-11-10 15:22:30 +0300

    commit

--- app.js
+++ app.js
@@ -37,9 +37,17 @@
 const express = require("express");
 const favicon = require("express-favicon");
 const fs = require("fs");
-const path = require("path");
+
+const ejs = require("ejs");
 
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const { addListener } = require("process");
+
 const port = "3000";
 
 const routeTest = "/test";
@@ -48,59 +56,106 @@ const routeSlash = "/";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
 app.use(favicon(__dirname + "/public/favicon.ico"));
-const filePath = path.join(__dirname, "tmp", "1.txt");
+const filePath = path.join(__dirname, "tmp", "logger.txt");
 fs.writeFile(filePath, "", (err) => {
   if (err) console.error(err);
   console.log("файл создан");
 });
-app.get(routeTest, (req, res) => {
-  fs.appendFile(
-    filePath,
-    `\nЛогируем ping по адресу localhost:${port}${routeTest}. Время: ${new Date()}`,
-    (err) => {
-      if (err) console.error(err);
-      console.log("файл переписан");
-    }
-  );
-  res.end("/test");
-});
-app.post(routeTest, (req, res) => {
-  console.log("Прошли по пути post/test");
-  fs.appendFile(
-    filePath,
-    `\nЛогируем ping по адресу localhost:${port}${routeTest}. Время: ${new Date()}`,
-    (err) => {
-      if (err) console.error(err);
-      console.log("файл переписан");
-    }
-  );
-  res.end("post/test");
-});
-console.log(__dirname + "/public/favicon.ico");
-app.get(routeSlash, function (req, res) {
-  res.end(
-    fs.appendFile(
-      filePath,
-      `\nЛогируем ping по адресу localhost:${port}${routeSlash}. Время: ${new Date()}`,
-      (err) => {
-        if (err) console.error(err);
-        console.log("файл переписан");
-      }
-    )
-  );
-});
-app.post(routeSlash, function (req, res) {
-  fs.appendFile(
-    filePath,
-    `\nЛогируем ping по адресу localhost:${port}${routeSlash}. Время: ${new Date()}`,
-    (err) => {
-      if (err) console.error(err);
-      console.log("файл переписан");
-    }
-  );
+// app.get(routeTest, (req, res) => {
+//   fs.appendFile(
+//     filePath,
+//     `\nЛогируем ping по адресу localhost:${port}${routeTest}. Время: ${new Date()}`,
+//     (err) => {
+//       if (err) console.error(err);
+//       console.log("файл переписан");
+//     }
+//   );
+//   res.end("/test");
+// });
+// app.post(routeTest, (req, res) => {
+//   console.log("Прошли по пути post/test");
+//   fs.appendFile(
+//     filePath,
+//     `\nЛогируем ping по адресу localhost:${port}${routeTest}. Время: ${new Date()}`,
+//     (err) => {
+//       if (err) console.error(err);
+//       console.log("файл переписан");
+//     }
+//   );
+//   res.end("post/test");
+// });
+// console.log(__dirname + "/public/favicon.ico");
+// app.get(routeSlash, function (req, res) {
+//   res.end(
+//     fs.appendFile(
+//       filePath,
+//       `\nЛогируем ping по адресу localhost:${port}${routeSlash}. Время: ${new Date()}`,
+//       (err) => {
+//         if (err) console.error(err);
+//         console.log("файл переписан");
+//       }
+//     )
+//   );
+// });
+// app.post(routeSlash, function (req, res) {
+//   fs.appendFile(
+//     filePath,
+//     `\nЛогируем ping по адресу localhost:${port}${routeSlash}. Время: ${new Date()}`,
+//     (err) => {
+//       if (err) console.error(err);
+//       console.log("файл переписан");
+//     }
+//   );
+// });
+
+// app.listen(port, () => {
+//   console.log(`listen on port ${port}`);
+// });
+
+app.get("/test", (req, res) => {});
+
+app.post("/test", (req, res) => {
+  addLine("Пинганули /test");
+  console.log("прошли по пути test");
+  res.end("прошли post test");
 });
 
-app.listen(port, () => {
-  console.log(`listen on port ${port}`);
+function addLine(line) {
+  line = line + " timestamp: " + new Date().toLocaleString();
+  fs.appendFile(
+    path.join(__dirname + "/tmp/logger.txt"),
+    line + "\n",
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+}
+
+//error handler
+app.use(function (req, res, next) {
+  const err = new Error("NO FOUND ERROR");
+  err.code = 404;
+  next(err);
+});
+
+//production error handler
+// console.log(app.get("env"));
+if (app.get("env") != "development") {
+  app.use(function (err, req, res, next) {
+    res.status = 404;
+    let photo =
+      "https://www.elegantthemes.com/blog/wp-content/uploads/2020/08/000-http-error-codes.png";
+    res.render("error", { err, photo });
+  });
+} else {
+  app.use(function (err, req, res, next) {
+    console.log(app.get("env"), err.code, err.message);
+  });
+}
+
+app.listen(port, function () {
+  console.log("Сервер запущен порт " + port);
+  addLine("Server started");
 });
